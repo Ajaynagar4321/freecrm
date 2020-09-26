138ef0165782897a4b83318822bc7d2e0fdcd3ec
Girish <Girish@DESKTOP-HSLV41C>
2020-09-26 16:40:06 +0530

    new

--- freecrmdemo/src/main/java/generic/BaseClass.java
+++ freecrmdemo/src/main/java/generic/BaseClass.java
@@ -1,18 +1,30 @@
 package generic;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class BaseClass {
 	public WebDriver driver;
 	public Properties p;
+	public ExtentReports report;
+	public ExtentTest test;
 	static {
 		System.setProperty("webdriver.chrome.driver", "./src/main/resources/drivernn/chromedriver.exe");
 	}
@@ -28,6 +40,34 @@ public class BaseClass {
 
 		driver.get(url);
 
+		// to get the report
+		report = new ExtentReports("./src/main/resources/reports/freecrm.html", false);
+		test = report.startTest("System testing");
+
+	}
+
+	@AfterMethod
+	public void closeBrowser(ITestResult res) throws IOException {
+
+		int result = res.getStatus();
+		String tc = res.getName();
+		// if test get failed it attached screenshot
+		if (result == 2) {
+
+			TakesScreenshot ts = (TakesScreenshot) driver;
+			File src = ts.getScreenshotAs(OutputType.FILE);
+			File dest = new File("./photo/" + tc + ".jpeg");
+			FileUtils.copyFile(src, dest);
+			String path = dest.getAbsolutePath();
+			test.log(LogStatus.FAIL, test.addScreenCapture(path));
+		}
+		// if test get pass
+		else if (result == 1) {
+			test.log(LogStatus.PASS, tc + " get passed");
+		}
+
+		report.endTest(test);
+		report.flush();
 	}
 
 }
